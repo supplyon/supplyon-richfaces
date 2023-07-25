11d30dd50f0d47b587b2e69643b706a0018633bf
viktar_patsiaichuk <viktar_patsiaichuk@epam.com>
2023-07-25 15:57:51 +0400

    Fix issue after assemble-scripts.xml renaming

--- framework/impl/src/main/java/org/richfaces/renderkit/html/scripts/scriptaculous/scriptaculous.js
+++ framework/impl/src/main/java/org/richfaces/renderkit/html/scripts/scriptaculous/scriptaculous.js
@@ -1,3 +1,3 @@
-// This file is assembled by generatescript.xml
-// As files concatenation: builder.js,effects.js,dragdrop.js,controls.js,slider.js,sound.js
+// This file is assembled by ANT script: assemble-scripts.xml
+// As files concatenation: builder.js, effects.js, dragdrop.js, controls.js, slider.js, sound.js
 throw new Error("scriptaculous.js was not assembled")
